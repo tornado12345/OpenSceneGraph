c296356483c18a343b337a6b1d82ed701892e292
Robert Osfield <robert@openscenegraph.com>
2016-10-20 20:35:03 +0100

    Intial work on providing mapping from OpenGL modes to #pragma(tic) shader composition defines.
    Shader  usage:
    
       #pragma import_modes ( GL_LIGHTING, GL_TEXGTURE_2D )
    
    Will provide #define in shaders for GL_LIGHTING and GL_TEXTURE_2D if these Modes are enabled via StateSet::setMode(..);

--- src/osg/StateSet.cpp
+++ src/osg/StateSet.cpp
@@ -1391,7 +1391,7 @@ const StateSet::RefAttributePair* StateSet::getTextureAttributePair(unsigned int
 
 bool StateSet::checkValidityOfAssociatedModes(osg::State& state) const
 {
-
+    OSG_NOTICE<<__PRETTY_FUNCTION__<<std::endl;
 
     bool modesValid = true;
     for(AttributeList::const_iterator itr = _attributeList.begin();
